04312c358cbfe273be69d4393d4cce274218e278
ChloeAshfiled <161962914+potatoprimeminister@users.noreply.github.com>
2026-02-23 20:20:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inputBox=document.getElementById("inputBox");
+constconst inputBox=document.getElementById("inputBox");
 const listContainer=document.getElementById("listContainer");
 
 function addTask(){
@@ -10,7 +10,34 @@ function addTask(){
         li.innerHTML=inputBox.value;
         listContainer.appendChild(li);
         
+        let span= document.createElement("span");
+        span.innerHTML='<i class="fa-solid fa-trash"></i>';
+        li.appendChild(span);
     }
 
     inputBox.value="";
+    saveData()
 }
+
+listContainer.addEventListener("click", function(e){
+
+    if(e.target.closest("li") && !e.target.closest("span")){
+        e.target.closest("li").classList.toggle("checked");
+        saveData()
+    }
+
+    if(e.target.closest("span")){
+        e.target.closest("li").remove();
+        saveData()
+    }
+
+});
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+
+}
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
