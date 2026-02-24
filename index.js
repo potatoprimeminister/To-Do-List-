0f2b0234a7ad2e2fae31710093b1b423989614d5
ChloeAshfiled <161962914+potatoprimeminister@users.noreply.github.com>
2026-02-24 09:45:17 +0700

    Refactor task management functions and event handling

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-constconst inputBox=document.getElementById("inputBox");
-const listContainer=document.getElementById("listContainer");
+const inputBox = document.getElementById("inputBox");
+const listContainer = document.getElementById("listContainer");
 
-function addTask(){
-    if(inputBox.value===''){
+function addTask() {
+    if (inputBox.value === '') {
         alert("You must write something");
-    }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        
-        let span= document.createElement("span");
-        span.innerHTML='<i class="fa-solid fa-trash"></i>';
+
+        let span = document.createElement("span");
+        span.innerHTML = '<i class="fa-solid fa-trash"></i>';
         li.appendChild(span);
     }
 
-    inputBox.value="";
-    saveData()
+    inputBox.value = "";
+    saveData();
 }
 
-listContainer.addEventListener("click", function(e){
+listContainer.addEventListener("click", function (e) {
+    const li = e.target.closest("li");
+    if (!li) return;
 
-    if(e.target.closest("li") && !e.target.closest("span")){
-        e.target.closest("li").classList.toggle("checked");
-        saveData()
+    if (e.target.closest("span")) {
+        li.remove();
+    } else {
+        li.classList.toggle("checked");
     }
 
-    if(e.target.closest("span")){
-        e.target.closest("li").remove();
-        saveData()
-    }
-
+    saveData();
 });
 
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML);
-
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
-function showTask(){
-    const savedData = localStorage.getItem("data");
-    if(savedData){
-        listContainer.innerHTML = savedData;
+
+function showTask() {
+    const data = localStorage.getItem("data");
+    if (data) {
+        listContainer.innerHTML = data;
     }
 }
-showTask();
 
+showTask();
